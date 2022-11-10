638ab6d0e7d62b6ef960416c1894ea51dbbe39ac
Robson De Andrade <theairelibre@protonmail.com>
2022-11-10 20:03:03 -0300

    feat: create interface Training

---
+++ src/app/interfaces/training.ts
@@ -0,0 +1,10 @@
+export interface Training {
+    id: string;
+    name: string;
+    summary: string;
+    instructor: string;
+    duration: number;
+    active: boolean;
+    author: string;
+    modules: Module[];
+}
