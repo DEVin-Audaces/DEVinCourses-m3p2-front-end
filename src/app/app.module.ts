9165622daff4209a4cc8164f7623199c4de17cc4
Vinicius Aloncio de Assunção <vinicius.assuncao@audaces.com>
2022-11-11 09:04:53 -0300

    add: adding the training screen component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { TrainingScreenComponent } from './components/training-screen/training-screen.component';
+import { ContentScreenComponent } from '../../content-screen/content-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TrainingScreenComponent,
+    ContentScreenComponent
   ],
   imports: [
     BrowserModule,
