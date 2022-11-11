47cdbaf0e0ea8e7b241babc2f8c2bf9dca9a26fd
Vinicius Aloncio de Assunção <vinicius.assuncao@audaces.com>
2022-11-11 15:15:45 -0300

    fix: fixing the import

--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PrivateRoutingModule } from './private-routing.module';
+import { TrainingScreenComponent } from 'src/app/components/training-screen/training-screen.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    TrainingScreenComponent,
+  ],
   imports: [
     CommonModule,
     SharedModule,
