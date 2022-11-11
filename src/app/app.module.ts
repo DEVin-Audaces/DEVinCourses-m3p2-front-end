70390252b177167d002d256c4953986ca37678f3
Vinicius Aloncio de Assunção <vinicius.assuncao@audaces.com>
2022-11-11 15:17:00 -0300

    fix: fixing the import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { TrainingScreenComponent } from './components/training-screen/training-screen.component';
-import { ContentScreenComponent } from './components/content-screen/content-screen.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TrainingScreenComponent,
-    ContentScreenComponent
   ],
   imports: [
     BrowserModule,
--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PrivateRoutingModule } from './private-routing.module';
+import { ContentScreenComponent } from 'src/app/components/content-screen/content-screen.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    ContentScreenComponent
+  ],
   imports: [
     CommonModule,
     SharedModule,
