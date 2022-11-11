7d12e6835212e425c87fea1fa918609cda09a330
Agnaldo Brito <agnaldo.jnr1995@gmail.com>
2022-11-11 09:11:45 -0300

    Feat: adding full page layout component and header

--- src/app/modules/private/private-routing.module.ts
+++ src/app/modules/private/private-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FullpageComponent } from './layout/fullpage/fullpage.component';
 
-const routes: Routes = [];
+// import { HomeComponent } from './components/home/home.component';
+// import { TrainingComponent } from './components/training/training.component';
+
+const routes: Routes = [
+  // {
+  //   path:'', component:FullpageComponent,
+  //   children:[
+  //     {path:'home', component:HomeComponent},
+  //     {path:'training', component: TrainingComponent}
+  //   ]
+  // }
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -3,12 +3,19 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PrivateRoutingModule } from './private-routing.module';
 import { FullpageComponent } from './layout/fullpage/fullpage.component';
+import { HeaderComponent } from './components/header/header.component';
+
+// import { HomeComponent } from './components/home/home.component';
+// import { TrainingComponent } from './components/training/training.component';
 
 
 
 @NgModule({
   declarations: [
-    FullpageComponent
+    FullpageComponent,
+    HeaderComponent,
+    // HomeComponent,
+    // TrainingComponent
   ],
   imports: [
     CommonModule,
