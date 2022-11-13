79b4bf4317624932a45e72724be459d2de902c38
Vinicius Aloncio de Assunção <vinicius.assuncao@audaces.com>
2022-11-13 14:57:57 -0300

    add: adding the content component and adding it to the training canvas

--- src/app/modules/private/private.module.ts
+++ src/app/modules/private/private.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PrivateRoutingModule } from './private-routing.module';
 import { TrainingScreenComponent } from 'src/app/components/training-screen/training-screen.component';
+import { ContentComponent } from 'src/app/components/content/content.component';
 
 
 
 @NgModule({
   declarations: [
-    TrainingScreenComponent
+    TrainingScreenComponent,
+    ContentComponent
   ],
   imports: [
     CommonModule,
