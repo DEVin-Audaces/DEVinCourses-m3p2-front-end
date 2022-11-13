c439f229c5b0cb464b9b47b63882bad0ea2b2cac
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-13 14:36:33 -0300

    refactor: change properties to nullable

--- src/app/interfaces/training.ts
+++ src/app/interfaces/training.ts
@@ -1,12 +1,12 @@
 import { Module } from "./module";
 
 export interface Training {
-    id: string;
-    name: string;
-    summary: string;
-    instructor: string;
-    duration: number;
-    active: boolean;
-    author: string;
-    modules: Module[];
+    id?: string;
+    name?: string;
+    summary?: string;
+    instructor?: string;
+    duration?: number;
+    active?: boolean;
+    author?: string;
+    modules?: Module[];
 }
