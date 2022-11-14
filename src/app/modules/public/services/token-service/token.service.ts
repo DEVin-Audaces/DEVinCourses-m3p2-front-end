f682588cee0b5579896d2914dc67f9d66c67d717
Matheus Gevartosky <matheus.gevartosky@audaces.com>
2022-11-14 15:13:54 -0300

    feat:

--- src/app/modules/public/services/token-service/token.service.ts
+++ src/app/modules/public/services/token-service/token.service.ts
@@ -1,4 +1,6 @@
 import { Injectable } from '@angular/core';
+import jwtDecode from 'jwt-decode';
+import { userJwtData } from 'src/app/interfaces/userJwtData';
 
 const KEY = 'bearer';
 
@@ -11,17 +13,28 @@ export class TokenService {
 
   public returnToken(): string {
     return localStorage.getItem(KEY) ?? '';
-}
+  }
 
-public saveToken(token: string): void {
+  public saveToken(token: string): void {
     localStorage.setItem(KEY, token);
-}
+  }
 
-public removeToken(): void {
+  public removeToken(): void {
     localStorage.removeItem(KEY);
-}
+  }
 
-public possuiToken(): boolean {
+  public hasToken(): boolean {
     return !!this.returnToken();
-}
+  }
+
+  public returnJwtData(): userJwtData{
+    const user: userJwtData = this.jwtDecode();
+    return user;
+  }
+
+  private jwtDecode(): userJwtData{
+    const token = this.returnToken();
+    const user: userJwtData = jwtDecode(token);
+    return user;
+  }
 }
