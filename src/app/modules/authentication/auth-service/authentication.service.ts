6654909487872a0aab631812a39b46ad6fca5f4f
Matheus Gevartosky <matheus.gevartosky@audaces.com>
2022-11-14 19:53:49 -0300

    feat: Create AuthModule and Auth Service

---
+++ src/app/modules/authentication/auth-service/authentication.service.ts
@@ -0,0 +1,34 @@
+import { HttpClient, HttpResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, tap } from 'rxjs';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+  private url = environment.API_URL ;
+
+  constructor(
+    private _http : HttpClient,
+    private _userService: UserService
+  ) { }
+
+  public autenticar(usuario: string, senha: string): Observable<HttpResponse<any>> {
+    return this._http
+        .post(
+            `${environment.API_URL}/user/login`,
+            {
+                userName: usuario,
+                password: senha,
+            },
+            { observe: 'response' }
+        )
+        .pipe(
+            tap((response) => {
+                const token = response.headers.get('x-access-token') ?? '';
+                this._userService.saveToken(token);
+            })
+        );
+}
+}
