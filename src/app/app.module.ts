a2c07f550c81da4c219534f539b5110a84eef688
Matheus Gevartosky <matheus.gevartosky@audaces.com>
2022-11-16 19:13:57 -0300

    feat: update app module imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthenticationInterceptor } from './interceptor/authentication.interceptor';
 
 @NgModule({
@@ -14,7 +14,8 @@ import { AuthenticationInterceptor } from './interceptor/authentication.intercep
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule
   ],
   providers: [
     {
