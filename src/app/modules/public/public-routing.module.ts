27601552980139ffc7132818e3bf4588200b6403
rodrigoraiche <rodrigo_raiche@estudante.sc.senai.br>
2022-11-18 18:56:59 -0300

    Feature: Create login component

--- src/app/modules/public/public-routing.module.ts
+++ src/app/modules/public/public-routing.module.ts
@@ -1,13 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import {LoginComponent} from "./components/login-component/login.component";
 
 const routes: Routes = [
-  { path:'', component: HomePageComponent},
   {
     path:'main',
     loadChildren:() => import('../private/private.module').then(m => m.PrivateModule)
-  }
+  },
+  {
+    path:'',
+    component: HomePageComponent,
+    children:[
+      { path: '', component: LoginComponent }
+    ]
+
+  },
 ];
 
 @NgModule({
--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -3,12 +3,14 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PublicRoutingModule } from './public-routing.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
+import { LoginComponent } from './components/login-component/login.component';
 
 
 
 @NgModule({
   declarations: [
-    HomePageComponent
+    HomePageComponent,
+    LoginComponent
   ],
   imports: [
     CommonModule,
