d75c0afc79cc2e5e21cfe26d5c554b57d5909b7f
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-19 20:03:43 -0300

    refactor: change communication between components

--- src/app/modules/private/trainings/create-training/create-training.component.ts
+++ src/app/modules/private/trainings/create-training/create-training.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { CreateTrainingService } from '../../services/create-training.service';
+import { Component, OnInit, ViewChild, ViewContainerRef } from '@angular/core';
 import { Training } from 'src/app/interfaces/training';
 import { Module } from 'src/app/interfaces/module';
 import { CreateModuleComponent } from '../create-module/create-module.component';
@@ -14,23 +15,46 @@ export class CreateTrainingComponent implements OnInit {
   @ViewChild(CreateRegisterComponent) RegisterChild!: CreateRegisterComponent;
   @ViewChild(CreateModuleComponent) ModuleChild!: CreateModuleComponent;
 
-  ngOnInit(): void {
-  }
-  
+  @ViewChild('moduleContainer', {read: ViewContainerRef}) moduleContainer!: ViewContainerRef;
+
+  public numberOfModules: number = 0;
+  public modules: Module[] = [];
+  public errorMessage: boolean = false;
   public displayCreateModule: boolean = false
   public training!: Training;
   public moduleArray: Module[] = []
 
-  
-  onSendTraining(): void {
-    this.displayCreateModule = true
+  constructor(private _createTrainingService: CreateTrainingService) {}
+
+  ngOnInit(): void {
+    this._createTrainingService
+      .getModule()
+      .subscribe(module => {
+        module.index = this.modules.length;
+        this.modules.push(module)
+      });
   }
 
-  public invokeChildren(): Training {
-    this.training = this.RegisterChild.getTraining()
-    this.moduleArray.push(this.ModuleChild.getModule())
-    this.training.modules = this.moduleArray
-    //console.log(this.training)
-    return this.training 
+  public addElement(): void {
+    this.moduleContainer.createComponent(CreateModuleComponent);
+    this.numberOfModules++;
+  }
+
+  // onSendTraining(): void {
+  //   this.displayCreateModule = true
+  // }
+
+  public invokeChildren(): void {
+    const isFormValid: boolean = this.RegisterChild.checkValidity();
+
+    if (isFormValid == false || this.numberOfModules != this.modules.length || this.numberOfModules == 0) {
+      this.errorMessage = true;
+      return;
+    }
+
+    let training = this.RegisterChild.getTraining();
+    training.modules = this.modules;
+
+    console.log(training); // TODO: implementar service
   }
 }
