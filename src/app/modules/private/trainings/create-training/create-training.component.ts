f3e5712c5d54e049e2433d795658769ff8fb80e5
Pedro Capinos <pedro.capinos@gmail.com>
2022-11-19 05:30:33 -0300

    squad3 feature: component create training

--- src/app/modules/private/trainings/create-training/create-training.component.ts
+++ src/app/modules/private/trainings/create-training/create-training.component.ts
@@ -1,5 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
 import { Training } from 'src/app/interfaces/training';
+import { Module } from 'src/app/interfaces/module';
+import { CreateModuleComponent } from '../create-module/create-module.component';
+import { CreateRegisterComponent } from '../create-register/create-register.component';
 
 @Component({
   selector: 'app-create-training',
@@ -8,13 +11,26 @@ import { Training } from 'src/app/interfaces/training';
 })
 export class CreateTrainingComponent implements OnInit {
 
-  constructor() { }
+  @ViewChild(CreateRegisterComponent) RegisterChild!: CreateRegisterComponent;
+  @ViewChild(CreateModuleComponent) ModuleChild!: CreateModuleComponent;
 
   ngOnInit(): void {
   }
+  
+  public displayCreateModule: boolean = false
+  public training!: Training;
+  public moduleArray: Module[] = []
 
-  onSendTraining(training: Training): void {
-    console.log(training);
+  
+  onSendTraining(): void {
+    this.displayCreateModule = true
   }
 
+  public invokeChildren(): Training {
+    this.training = this.RegisterChild.getTraining()
+    this.moduleArray.push(this.ModuleChild.getModule())
+    this.training.modules = this.moduleArray
+    //console.log(this.training)
+    return this.training 
+  }
 }
