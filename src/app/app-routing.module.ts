524b0636dc6005f9f9b819aaf93be4bf5c846589
matheusgevartosky <magevartosky@gmail.com>
2022-11-20 18:11:41 -0300

    feat/ update routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthenticationGuard } from './guard/authentication.guard';
+import { LoginGuard } from './guard/login.guard';
 
 const routes: Routes = [
+  {path: "", pathMatch: 'full', redirectTo: 'login'},
   {
-    path:'',
-    loadChildren:() => import('../app/modules/public/public.module').then(m => m.PublicModule)
+    path:'trainings',
+    loadChildren:() => import('../app/modules/private/private.module').then(m => m.PrivateModule),
+    canLoad: [AuthenticationGuard]
+  },
+  {
+    path:'login',
+    loadChildren:() => import('../app/modules/public/public.module').then(m => m.PublicModule),
+    canLoad: [LoginGuard]
   }
+
 ];
 
 @NgModule({
