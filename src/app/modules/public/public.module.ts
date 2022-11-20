6223a8b2f8c77781e7e8de8cdc770bda6588c1e2
Matheus Gevartosky <86495867+matheusgevartosky@users.noreply.github.com>
2022-11-20 16:06:08 -0300

    Update public.module.ts

--- src/app/modules/public/public.module.ts
+++ src/app/modules/public/public.module.ts
@@ -3,7 +3,7 @@ import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { PublicRoutingModule } from './public-routing.module';
 import { HomePageComponent } from './components/home-page/home-page.component';
-import { LoginComponent } from './components/login-component/login/login.component';
+import { LoginComponent } from './components/login-component/login.component';
 import { ChangePasswordComponent } from './components/changePasswordModal/change-password.component';
 import { ModalService } from './services/modal-service/modal.service';
 import { NewUserPageComponent } from './components/new-user-page/new-user-page.component';
