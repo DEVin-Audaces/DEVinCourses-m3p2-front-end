7794eaa097d732ae2d9a175408d006853334ae34
gmvilela <gm.vilela@uol.com.br>
2022-11-20 17:36:50 -0300

    feat: create service getalltrainings

--- src/app/modules/private/services/trainings.service.ts
+++ src/app/modules/private/services/trainings.service.ts
@@ -2,15 +2,24 @@ import { Report } from './../../../interfaces/report';
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { Training } from 'src/app/interfaces/training';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TrainingsService {
 
+  private url = `${environment.API_URL}/trainings`;
+
   constructor(private _http: HttpClient) { }
 
   public getReport(): Observable<Report[]> {
     return this._http.get<Report[]>('https://localhost:7200/trainings/report');
   }
+
+  public getAllTrainings(): Observable<Training[]> {
+    return this._http.get<Training[]>(this.url);
+  }
+
 }
