670121d819c8b643b56d3a73fea9752636669cd7
Lucas Reibnitz <lucas.fpolis@gmail.com>
2022-11-20 22:01:16 -0300

    fix: landing page path

--- src/app/modules/private/private-routing.module.ts
+++ src/app/modules/private/private-routing.module.ts
@@ -3,7 +3,6 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FullpageComponent } from './layout/fullpage/fullpage.component';
 import { TrainingRegistrationComponent } from './trainings/training-registration/training-registration.component';
-import { TrainingScreenComponent } from './trainings/training-screen/training-screen.component';
 import { CreateTrainingComponent } from './trainings/create-training/create-training.component';
 import { InitialScreenComponent } from './trainings/initial-screen/initial-screen.component';
 import { ReportListComponent } from './trainings/report-list/report-list.component';
@@ -13,7 +12,7 @@ const routes: Routes = [
   {
     path: '', component: FullpageComponent,
     children: [
-      { path: 'home', component: InitialScreenComponent },
+      { path: '', component: InitialScreenComponent },
       { path: 'registration/:id', component: TrainingRegistrationComponent },
       { path: 'report', component: ReportListComponent },
       { path: 'create-training', component: CreateTrainingComponent },
